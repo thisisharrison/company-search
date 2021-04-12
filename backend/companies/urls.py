79fd3cd25c136ea43e65947af2d057ddf3a57f3e
thisisharrison <harrison.lau@outlook.com>
2021-04-12 18:35:53 +0800

    Companies rest framework

---
+++ backend/companies/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+  path('api/companies/', views.CompanyListCreate.as_view() ),
+]
--- backend/companies/views.py
+++ backend/companies/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import Company
+from .serializers import CompanySerializer
+from rest_framework import generics
 
 # Create your views here.
+
+# GET and POST requests
+class CompanyListCreate(generics.ListCreateAPIView):
+  queryset = Company.objects.all()
+  serializer_class = CompanySerializer
